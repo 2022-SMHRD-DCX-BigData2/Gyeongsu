867e276ffa05eee112eeb7c14167bdbe3cd6d5c7
baeo <ghwn5908@gmail.com>
2022-11-22 10:10:38 +0900

    디자인 다시시도

---
+++ gittest/src/main/webapp/assets/js/index.js
@@ -0,0 +1,50 @@
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabcon = document.querySelectorAll("[data-tab-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabcon.forEach((tabc_all) => {
+      tabc_all.classList.remove("active");
+    });
+
+    target.classList.add("active");
+  });
+});
+// const tabs2 = document.querySelectorAll("[data-tab-target2]");
+// const tabcon2 = document.querySelectorAll("[data-tab-content2]");
+
+// tabs2.forEach((tab) => {
+//     tab.addEventListener("click", () => {
+//       const target = document.querySelector(tab.dataset.tabTarget);
+//       tabcon2.forEach((tabc_all) => {
+//         tabc_all.classList.remove("active");
+//       });
+  
+//       target.classList.add("active");
+//     });
+//   });
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
+const exampleModal = document.getElementById('exampleModal')
+exampleModal.addEventListener('show.bs.modal', event => {
+  // Button that triggered the modal
+  const button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  const recipient = button.getAttribute('data-bs-whatever')
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  const modalTitle = exampleModal.querySelector('.modal-title')
+  const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+  modalTitle.textContent = `New message to ${recipient}`
+  modalBodyInput.value = recipient
+})
